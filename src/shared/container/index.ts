d79e2fe71c09961de7eedbff57673618ad26c85d
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-07-22 12:05:39 -0300

    ✨ feat: auth middleware

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
-import { UserRepository } from "@modules/account/infra/typeorm/repositories/UserRepository";
-import { IUsersRepository } from "@modules/account/repositories/IUserRepository";
 import { container } from "tsyringe";
+import { UserRepository } from "@modules/account/infra/typeorm/repositories/UserRepository";
+import { UserTokensRepository } from "@modules/account/infra/typeorm/repositories/UserTokensRepository";
+import { IUsersRepository } from "@modules/account/repositories/IUserRepository";
+import { IUsersTokensRepository } from "@modules/account/repositories/IUserTokensRepository";
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UserRepository
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+  "UserTokensRepository",
+  UserTokensRepository
 );
---
+++ src/shared/infra/http/middlewares/ensureAuth.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import authConfig from "@config/authConfig";
+import { AppError } from "@shared/errors/AppError";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuth(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("the token is missing", 401);
+  }
+
+  const [, token] = authHeader.split("");
+
+  try {
+    const { sub: user_id } = verify(token, authConfig.secret_token) as IPayload;
+
+    req.user = {
+      id: user_id,
+    };
+  } catch (error) {
+    throw new AppError("invalid token", 401);
+  }
+}

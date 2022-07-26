249172fc22613637dfb82225cb91d968cbc87151
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-07-26 11:34:48 -0300

    ✨ feat: add ticket routes

---
+++ src/shared/infra/http/routes/tickets.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { CreateTicketController } from "@modules/tickets/useCases/createTicket/createTicketController";
+import { DeleteTicketController } from "@modules/tickets/useCases/deleteTicket/deleteTicketController";
+import { FindTicketController } from "@modules/tickets/useCases/findTicket/FindTicketController";
+import { FindAllTicketsController } from "@modules/tickets/useCases/findAllTickets/findAllTicketsController";
+import { ensureAuth } from "../middlewares/ensureAuth";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+
+const ticketsRoutes = Router();
+
+const createTicketController = new CreateTicketController();
+const deleteTicketController = new DeleteTicketController();
+const findTicketController = new FindTicketController();
+const findAllTicketsController = new FindAllTicketsController();
+
+ticketsRoutes.post("/", createTicketController.handle);
+
+ticketsRoutes.delete(
+  "/:id",
+  ensureAuth,
+  ensureAdmin,
+  deleteTicketController.handle
+);
+
+ticketsRoutes.get("/:id", ensureAuth, findTicketController.handle);
+
+ticketsRoutes.get("/", findAllTicketsController.handle);
+
+export { ticketsRoutes };
